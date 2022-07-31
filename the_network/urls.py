797c06f7843a897c60f87f706de63bb3891a22af
Hira Khan <phr.hirakhan@gmail.com>
2022-07-31 17:38:34 +0200

    connecting a view to a template

--- the_network/urls.py
+++ the_network/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('<int:num_pg>', views.page_num_view),
-    path('<str:topic>/', views.news, name='topic-page')
+    path('', views.simple_view)  # domain.com/network
 ]
--- the_network/views.py
+++ the_network/views.py
@@ -1,27 +1,10 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.urls import reverse
+
+
 # Create your views here.
 
-articles = {
-    'sports': 'Sports Page',
-    'finance': 'Finance Page',
-    'politics': 'Politics Page'
-}
-
-
-def news(request, topic):
-    try:
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404('GENERIC ERROR')
-
-
-def page_num_view(request, num_pg):
-    topics_list = list(articles.keys())  # ['sports', 'finance', 'politics']#
-    topic = topics_list[num_pg]
-
-    webpage = reverse('topic-page', args=[topic])
-    return HttpResponseRedirect(webpage)
-
-
+# Connecting a view to a template
+def simple_view(request):
+    return render(request, 'the_network/example.html')
